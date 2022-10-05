7167a729eaa736be1b7bb40ed8aa2af015f0e162
Admin <Admin@TEB123011>
2022-10-05 11:39:37 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Uppgift_3._15
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar ska du hyra bilen?");
+            Console.Write("Svara här: "); int timmar = int.Parse(Console.ReadLine());
+
+            if( timmar  >11)
+            {
+                Console.WriteLine("Du får tyvär inte hyra bilen mer än 11 timmar");
+            }
+             
+            if (timmar < 12)
+            {
+                Console.WriteLine("Det kommer att kosta dig" + timmar * 80 + "Koronor");
+            }
+
+        }
+    }
+}
